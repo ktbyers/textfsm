7e90011ed9abbd2c51aab650c39e94e155526855
Kirk Byers <ktbyers@twb-tech.com>
2018-10-19 17:50:54 -0700

    Add some debugging colors

--- textfsm.py
+++ textfsm.py
@@ -35,6 +35,10 @@ import inspect
 import re
 import string
 import sys
+import colorama
+
+
+DEBUG = True
 
 
 class Error(Exception):
@@ -656,6 +660,9 @@ class TextFSM(object):
       cur_record[cur_record.index(None)] = ''
 
     self._result.append(cur_record)
+    if DEBUG:
+        print("{}{}: {}".format(colorama.Fore.BLUE, "RECORD > ", cur_record))
+        print(colorama.Style.RESET_ALL)
     self._ClearRecord()
 
   def _Parse(self, template):
@@ -930,6 +937,9 @@ class TextFSM(object):
       if matched:
         for value in matched.groupdict():
           self._AssignVar(matched, value)
+          if DEBUG:
+            print("{}{}: {}".format(colorama.Fore.GREEN, "Assign Var > ", value))
+            print(colorama.Style.RESET_ALL)
 
         if self._Operations(rule, line):
           # Not a Continue so check for state transition.
@@ -937,6 +947,9 @@ class TextFSM(object):
             if rule.new_state not in ('End', 'EOF'):
               self._cur_state = self.states[rule.new_state]
             self._cur_state_name = rule.new_state
+            if DEBUG:
+              print("{}{}: {}".format(colorama.Fore.RED, "State Transition > ", rule.new_state))
+              print(colorama.Style.RESET_ALL)
           break
 
   def _CheckRule(self, rule, line):
